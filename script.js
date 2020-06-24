ada68b276682b230a4b9fc66c064cb3405e97122
Bunea Rares <bunearaes@gmail.com>
2020-06-25 02:47:47 +0300

    Added new functions,the creatorDiv() loop is broken.

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const button = document.querySelector(`button`);
+const gameContainer = document.querySelector(`#game_container`);
+//Ask user how many size && start the creatorDiv
+let promptToStart = () => {
+  let numberOfDivs = prompt(`How many divs?`);
+  creatorDiv(numberOfDivs);
+};
+//Takes input from promptToStart() and creates the game size
+let creatorDiv = (numberDiv) => {
+  for (let i = numberDiv; i < numberDiv; i++) {
+    let div = document.createElement(`div`);
+    gameDiv.append(div);
+    console.log(`red`);
+  }
+};
 
-button.addEventListener("click", test;
-
-let test = () => console.log(`working`);
+button.addEventListener(`click`, promptToStart);
