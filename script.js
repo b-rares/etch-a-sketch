5484e96b22f92b4d460be28e9b1ea583adb02548
Bunea Rares <bunearaes@gmail.com>
2020-06-27 21:44:15 +0300

    Rewrite the script and modified the style

--- script.js
+++ script.js
@@ -8,30 +8,18 @@ const colors = [
   `#DEC4A1`,
   `#EDCF8E`,
 ];
-//Ask user how many size && start the creatorDiv
-let promptToStart = () => {
-  let numberOfDivs = prompt(`How many divs?`);
-  creatorDiv(numberOfDivs);
-};
-//Takes input from promptToStart() and creates the game size
-let creatorDiv = (numberDiv) => {
-  let j = 0;
-  for (let i = 0; i < numberDiv; i++) {
-    j++;
-    let div = document.createElement(`div`);
-    div.classList.add(`div-game`);
-    div.id = `div-game${i}`;
-    divContainer.append(div);
-  }
-  const div_game = document.querySelectorAll(`.div-game`);
-  return div_game;
-};
+let gridSize = 16;
 
-//Randomize the colors and gives one to the div
-let changeColor = () => {
-  div_id.style.backgroundColor =
-    colors[Math.floor(Math.random() * colors.length)];
+let createGrid = () => {
+  divContainer.style.gridTemplateColumns = `repeat (${gridSize},1fr)`;
+  divContainer.style.gridTemplateRows = `repeat (${gridSize},1fr)`;
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    let div = document.createElement(`div`);
+    div.className.add = `div-game`;
+    div.addEventListener(`mouseenter`, function () {
+      div.style.backgroundColor =
+        colors[Math.floor(Math.random * colors.length)];
+    });
+    divContainer.appendChild(div);
+  }
 };
-//EventsListeners;
-button.addEventListener(`click`, promptToStart);
-div_game.addEventListener(`onmouseover`, changeColor(creatorDiv()));
