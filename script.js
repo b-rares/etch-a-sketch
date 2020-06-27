f2d26c3fb1d151007417046e90f5d67b148a1b91
Bunea Rares <bunearaes@gmail.com>
2020-06-27 15:48:45 +0300

    Added new functions and style

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const button = document.querySelector(`button`);
-const gameContainer = document.querySelector(`#game_container`);
+const divContainer = document.querySelector(`#div_container`);
+const colors = [
+  `#BA7BA1`,
+  `#C28CAE`,
+  `#D0ABA0`,
+  `#DEC4A1`,
+  `#DEC4A1`,
+  `#EDCF8E`,
+];
 //Ask user how many size && start the creatorDiv
 let promptToStart = () => {
   let numberOfDivs = prompt(`How many divs?`);
@@ -7,11 +15,19 @@ let promptToStart = () => {
 };
 //Takes input from promptToStart() and creates the game size
 let creatorDiv = (numberDiv) => {
-  for (let i = numberDiv; i < numberDiv; i++) {
+  for (let i = 0; i < numberDiv; i++) {
     let div = document.createElement(`div`);
-    gameDiv.append(div);
-    console.log(`red`);
+    div.classList.add(`div-game`);
+    divContainer.append(div);
   }
+  const div_game = document.querySelectorAll(`.div-game`);
+  return div_game;
 };
-
+//Randomize the colors and gives one to the div
+let changeColor = () => {
+  div_game.style.backgroundColor =
+    colors[Math.floor(Math.random() * colors.length)];
+};
+//EventsListeners
 button.addEventListener(`click`, promptToStart);
+div_game.addEventListener(`onmouseover`, changeColor);
